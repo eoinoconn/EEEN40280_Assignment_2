7a77004e52d2a2ff4626cafa12900ec1f550611b
Conor Martin <conor.martin.1@ucdconnect.ie>
2018-03-05 16:37:11 +0000

    added modes to display zero-to-peak and peak-to-peak amps, not complete, IE and T2CON need values

--- Source/main.c
+++ Source/main.c
@@ -27,7 +27,11 @@ typedef unsigned long int uint32;
 
 static uint8 mode;
 static uint16 samp;
+static uint16 samp_max;
+static uint16 samp_min;
 static uint16 average;
+static uint16 average_max;
+static uint16 average_min;
 static uint16 message;
 
 
@@ -48,13 +52,18 @@ void ADC1 (void) interrupt 6 using 2
 	samp = ADCDATAH & 0xF;												// extract the most significant bits 
 	samp = ((samp << 8) + ADCDATAL);							// store sample value
 	average = (samp >> 2) + ((average >> 2) * 3);	// calculate running average
+	
+	if (samp > 2048) samp_max = samp;	// schmitt trigger output is high, so value attributed to high peak 
+	else samp_min = samp;							// schmitt trigger output is low, so value attributed to low peak
+	average_max = (samp_max >> 2) + ((average_max >> 2) * 3);	// calculate running average of high peak value
+	average_min = (samp_min >> 2) + ((average_min >> 2) * 3);	// calculate running average of low peak value
 }
 
 
 void delay (uint16 delayVal)
 {
-	uint16 i, j;                 // counting variable 
-	for (i = 0; i < delayVal; i++)    // repeat  
+	uint16 i, j;
+	for (i = 0; i < delayVal; i++)
     {
 		  for(j=0; j < 100; j++)
 			{
@@ -66,7 +75,7 @@ void delay (uint16 delayVal)
 
 void send_message(uint8 addr, uint8 instr)
 {
-	volatile uint8 dummy;										// used to delay between transfers
+	volatile uint8 dummy;						// used to delay between transfers
 	LOAD = 0;
 	
 	SPIDAT = addr & 0xF;						// load addr into shift register
@@ -101,15 +110,15 @@ void disp_setup()
 }
 
 
-void disp_value(uint32 value)
+void disp_value(uint32 value, uint8 dpoint)
 {
 	uint8 i, digit;
 	for (i = 1; i <= 8; i++)
 	{
 		digit = value % 10;									// get value of current digit
-		if(i == 4) digit = digit | 0x80;	// include decimal pt for leftmost digit to convert to V
-		send_message(i,digit);						// update digit on display
-		value /= 10;													// move to next digit
+		if((i == 4) && (dpoint == 1)) digit = digit | 0x80;		// include decimal pt for leftmost digit to convert to V
+		send_message(i,digit);							// update digit on display
+		value /= 10;												// move to next digit
 	}
 }
 
@@ -117,17 +126,19 @@ void disp_value(uint32 value)
 
 void main (void)
 {
+	// Setup
 	uint32 display_value;
+	uint8 dpoint;
 	P2 = 0xFF;
 	ADCCON1 = 0xB2;		// setup the ADC
 	disp_setup();			// Call display setup function
-
+	// Main Loop
 	while (1)
 	{
 		////////////////////////////////////////////////////
 		// DC mode
 		////////////////////////////////////////////////////
-		if (P2 == 0)					
+		if (P2 == 0x00)					
 		{
 			
 			IE = 192;							// enable only the ADC interrupt
@@ -136,23 +147,64 @@ void main (void)
 			T2CON = 0x4;					// setup timer 2
 			send_message(11,3);		// set 4 rightmost digits active
 
-
-			display_value = (average*625L) >> 10;	// scale adc_val by 625/1024 ~= 0.61 to get voltage in mV
+			display_value = (average*625L) >> 10;	// scale adc_val by 2500/4096 = 625/1024 ~= 0.61 to get voltage in mV
+			dpoint = 1;
 		}
+		
 		////////////////////////////////////////////////////
 		// Frequency Mode
 		////////////////////////////////////////////////////
-		else if (P2 == 1)			
+		else if (P2 == 0x01)			
 		{
 			IE = 160;							// enable only the ADC interrupt
 			RCAP2L = 0;						// reload high byte of timer 2
 			RCAP2H = 0;						// reload high byte of timer 2
 			T2CON = 0xF;					// setup timer 2
-			send_message(11,7);		// set 8 rightmost digits active
-			
-			
-			display_value = 5530973L/average;	// calculate value to display			
+			send_message(11,4);		// set 5 rightmost digits active
+					
+			display_value = 5530973L/average;	// calculate value to display
+			dpoint = 0;
 		}
+		
+		////////////////////////////////////////////////////
+		// Zero-to-Peak Amplitude mode
+		////////////////////////////////////////////////////
+		else if (P2 == 0x02)
+		{
+			IE = TODO;							// enable only the ADC interrupt
+			RCAP2L = 214;					// reload high byte of timer 2
+			RCAP2H = 213;					// reload high byte of timer 2
+			T2CON = TODO;					// setup timer 2
+			send_message(11,3);		// set 4 rightmost digits active
+			
+			display_value = 2*(average_max - 2048); // display distance between peak and level shifted 'zero', scaled by 1/0.5
+			dpoint = 1;
+		}
+		
+		////////////////////////////////////////////////////
+		// Peak-to-Peak Amplitude mode
+		////////////////////////////////////////////////////
+		else if (P2 == 0x03)
+		{
+			IE = TODO;							// enable only the ADC interrupt
+			RCAP2L = 214;					// reload high byte of timer 2
+			RCAP2H = 213;					// reload high byte of timer 2
+			T2CON = TODO;					// setup timer 2
+			send_message(11,3);		// set 4 rightmost digits active
+			
+			display_value = 2*(average_max - average_min); // display distance between high peak and low peak, scaled by 1/0.5
+			dpoint = 1;
+			
+		}
+		
+		////////////////////////////////////////////////////
+		// Pseudo-RMS Amplitude mode
+		////////////////////////////////////////////////////
+		//else if (P2 == 0x04)
+		//{
+		//	
+		//}
+		
 		disp_value(display_value);
 		delay(13107);
 	}
