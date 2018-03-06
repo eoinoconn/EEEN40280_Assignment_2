188ad891a5a7e9ddef221aaf3b357047fc382c4c
Eoin <eoinoconn@gmail.com>
2018-03-06 13:21:11 +0000

    fixes

--- Source/main.c
+++ Source/main.c
@@ -25,26 +25,45 @@ typedef unsigned long int uint32;
 
 #define LOAD T0
 
-static uint8 mode;
-static uint16 samp;
-static uint16 average;
+static uint8 overflows;
+static uint16 last_samp;
+static uint32 average;
 static uint16 message;
 
 
 // timer interrupt
 void timer2 (void) interrupt 5 using 1
 {
-	TF2 = 0;
-	EXF2 = 0;
-	samp = RCAP2H;																// extract the most significant bits 
-	samp = ((samp << 8) + RCAP2L);								// store sample value
-	average = (samp >> 2) + ((average >> 2) * 3);	// calculate running average
+	if (TF2 & EXF2)
+	{
+		uint32 samp;
+		uint16 capture;
+		
+		// get sample of counts occuring between periods
+		capture = RCAP2H;																		// extract the most significant bits 
+		capture = ((capture << 8) + RCAP2L);								// store sample value
+		
+		samp = capture-last_samp + (overflows*65535L);			// find difference between last capture and current to get number of cycles completed
+		
+		average = (samp >> 2) + ((average >> 2) * 3);				// calculate running average
+		
+		last_samp = capture;																// save the current capture to calculate the sample on the next flag
+		overflows = 0;																			// reset the number of overflows
+	}
+	else
+	{
+		overflows++;																// if only the timer flag rasied an overflow has occured
+	}
+	TF2 = 0;																			// Reset timer flag, not done by hardware
+	EXF2 = 0;																			// Reset external event flag, not done by hardware
 }
 
 // ADC interrupt
 void ADC1 (void) interrupt 6 using 2
 {
+	uint16 samp;
 	TF2 = 0;																			// Reset timer flag, not done by hardware
+	// get sample of ADC
 	samp = ADCDATAH & 0xF;												// extract the most significant bits 
 	samp = ((samp << 8) + ADCDATAL);							// store sample value
 	average = (samp >> 2) + ((average >> 2) * 3);	// calculate running average
@@ -106,9 +125,9 @@ void disp_value(uint32 value)
 	uint8 i, digit;
 	for (i = 1; i <= 8; i++)
 	{
-		digit = value % 10;									// get value of current digit
-		if(i == 4) digit = digit | 0x80;	// include decimal pt for leftmost digit to convert to V
-		send_message(i,digit);						// update digit on display
+		digit = value % 10;										// get value of current digit
+		if(i == 4) digit = digit | 0x80;			// include decimal pt for leftmost digit to convert to V
+		send_message(i,digit);								// update digit on display
 		value /= 10;													// move to next digit
 	}
 }
@@ -119,6 +138,7 @@ void main (void)
 {
 	uint32 display_value;
 	P2 = 0xFF;
+	average = 0;
 	
 	disp_setup();			// Call display setup function
 
@@ -129,7 +149,7 @@ void main (void)
 		////////////////////////////////////////////////////
 		if (P2 == 0)					
 		{
-			ADCCON1 = 0xB2;			// setup the ADC
+			ADCCON1 = 0xB2;				// setup the ADC
 			IE = 192;							// enable only the ADC interrupt
 			RCAP2L = 214;					// reload high byte of timer 2
 			RCAP2H = 213;					// reload high byte of timer 2
@@ -144,14 +164,21 @@ void main (void)
 		////////////////////////////////////////////////////
 		else if (P2 == 1)			
 		{
-			ADCCON1 = 0;		// setup the ADC
+			ADCCON1 = 0;					// setup the ADC
 			IE = 160;							// enable only the ADC interrupt
 			T2CON = 0xD;					// setup timer 2
 			send_message(11,7);		// set 8 rightmost digits active
 			
-			
-			display_value = 5530973L/average;	// calculate value to display			
+			if ((average < 110) | (average > 11100000))
+			{
+				// out of range error
+			}
+			else
+			{
+			display_value = 5530973L/average;	// calculate value to display
+			}
 		}
+
 		disp_value(display_value);
 		delay(1310);
 	}
